4e84f520d65301a4932ca0eec450c4f88fd03b00
Lucas Sequeira <lucas.sequeira@yahoo.com.ar>
2020-09-20 19:05:37 -0300

    Se borraron las rutas que no se utilizan

--- src/config/server.js
+++ src/config/server.js
@@ -5,9 +5,6 @@ import baseTecnicaRouter from '../routes/baseTecnica';
 import dispositivoRouter from '../routes/Dispositivo';
 import usuarioRouter from '../routes/usuario';
 import estadoRouter from '../routes/estado';
-//import perfilRouter from '../routes/perfil';
-//import organizacionRouter from '../routes/organizacion';
-//import smartGroupRouter from '../routes/smartGroup';
 import modeloRouter from '../routes/modelo';
 import authRouter from '../routes/auth';
 class App {
@@ -29,9 +26,6 @@ class App {
         this.express.use('/api/Dispositivo',dispositivoRouter);
         this.express.use('/api/usuario',usuarioRouter);
         this.express.use('/api/estado',estadoRouter);
-        //this.express.use('/api/perfil',perfilRouter);
-        //this.express.use('/api/organizacion',organizacionRouter),
-        //this.express.use('/api/smartGroup',smartGroupRouter),
         this.express.use('/api/modelo',modeloRouter),
         this.express.use('/api/auth',authRouter)
     }
